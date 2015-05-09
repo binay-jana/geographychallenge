91ce655c5776c47be1b928b49cf940e6408b1e74
binay <binay@jana.com>
2015-05-09 15:46:50 -0400

    changed some css

--- flask_challenge_app/blueprints/main.py
+++ flask_challenge_app/blueprints/main.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8; -*-
 from flask import Blueprint, render_template
-# from libraries.Log import Log
+from libraries.Log import Log
 
 main = Blueprint('main', __name__)
 
-# _logger = Log.getLogger(__name__)
+_logger = Log.getLogger(__name__)
 
 
 @main.route('/')
